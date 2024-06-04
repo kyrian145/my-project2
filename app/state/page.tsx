a0355d7151c6200ee3d12a6b379c283bfb6ee8d8
unknown <ugwukyrianebuka@gmail.com>
2024-06-04 10:00:44 -0700

    feat: Loading, error, routes

---
+++ app/state/page.tsx
@@ -0,0 +1,14 @@
+const state = ()=>{
+    return(
+        <div>
+            <p>I am a good </p>
+            <div>
+                <p>sfjf</p>
+                <p>jda fj</p>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
